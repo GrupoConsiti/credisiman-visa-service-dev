9a30300ae92f3bf0d102fe736aeabddef73b8966
carlossalazar7 <carlossalazar2228@gmail.com>
2022-12-15 16:07:52 -0600

    Servicio BloqueoDesbloqueoTarjeta

--- src/main/java/com/siman/credisiman/visa/service/BloqueoDesbloqueoTarjeta.java
+++ src/main/java/com/siman/credisiman/visa/service/BloqueoDesbloqueoTarjeta.java
@@ -1,22 +1,30 @@
 package com.siman.credisiman.visa.service;
 
-import org.apache.xmlbeans.XmlCursor;
-import org.apache.xmlbeans.XmlObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.siman.credisiman.visa.dto.bloqueodesbloqueotarjeta.BloqueoDesbloqueoTarjetaResponse;
+import com.siman.credisiman.visa.utils.ConnectionHandler;
 import com.siman.credisiman.visa.utils.Message;
 import com.siman.credisiman.visa.utils.Utils;
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlObject;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.xml.namespace.QName;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class BloqueoDesbloqueoTarjeta {
     private static final Logger log = LoggerFactory.getLogger(BloqueoDesbloqueoTarjeta.class);
+    private static final String namespace = "http://siman.com/BloqueoDesbloqueoTarjeta";
+    private static final String operationResponse = "ObtenerBloqueoDesbloqueoTarjetaResponse";
 
-	public static XmlObject obtenerBloqueoDesbloqueoTarjeta(String pais, String numeroTarjeta, String estadoDeseado,
-			String motivo, String remoteJndiSunnel, String remoteJndiOrion, String siscardUrl, String siscardUser,
-			String binCredisiman, String tipoTarjeta) {
-        String namespace = "http://siman.com/BloqueoDesbloqueoTarjeta";
-        String operationResponse = "ObtenerBloqueoDesbloqueoTarjetaResponse";
+    public static XmlObject obtenerBloqueoDesbloqueoTarjeta(String pais, String numeroTarjeta, String estadoDeseado,
+                                                            String motivo, String remoteJndiSunnel, String remoteJndiOrion, String siscardUrl, String siscardUser,
+                                                            String binCredisiman, String tipoTarjeta) {
         //OBTENER DATOS
 
         //validar campos requeridos
@@ -51,17 +59,176 @@ public class BloqueoDesbloqueoTarjeta {
                     "La longitud del campo número tarjeta debe ser menor o igual a 16", namespace, operationResponse);
         }
 
+        try {
+            switch (tipoTarjeta) {
+                case "P":
+                    //datos tarjeta privada
+                    BloqueoDesbloqueoTarjetaResponse response1= null;
+                    response1 = obtenerDatosArca(numeroTarjeta, estadoDeseado, remoteJndiSunnel);
+                    if (response1 != null) {
+                        return estructura(response1);
+                    } else {
+                        return message.genericMessage("ERROR", "400", "Tarjeta no encontrada ", namespace, operationResponse);
+                    }
+                case "V":
+                    //datos tarjeta visa
+                    BloqueoDesbloqueoTarjetaResponse response2 = obtenerDatosSiscard(pais, numeroTarjeta, estadoDeseado, motivo, siscardUrl);
+                    if (response2 != null) {
+                        return estructura(response2);
+                    } else {
+                        return message.genericMessage("ERROR", "400", "Tarjeta no encontrada ", namespace, operationResponse);
+                    }
+                default:
+                    return message.genericMessage("ERROR", "400", "Tipo Tarjeta no valida", namespace, operationResponse);
 
+            }
+        } catch (Exception e) {
+            log.info("ObtenerBloqueoDesbloqueoTarjeta response = [" + message.genericMessage("ERROR", "600", "Error general contacte al administrador del sistema...", namespace, operationResponse) + "]");
+            return message.genericMessage("ERROR", "600", "Error general contacte al administrador del sistema...", namespace, operationResponse);
+        }
+
+    }
+
+    public static BloqueoDesbloqueoTarjetaResponse obtenerDatosArca(String numeroTarjeta, String estadoDeseado,
+                                                                    String remoteJndiSunnel) throws Exception {
+        BloqueoDesbloqueoTarjetaResponse response = new BloqueoDesbloqueoTarjetaResponse();
+//        BLOQUEO TEMPORAL (estadoDeseado 1)
+        String query1 = "UPDATE SUNNELP3.t_gcard SET riskconditionind = 'T', riskconditiondate = CURRENT_DATE, " +
+                "riskcondreasoncodeid = 1 WHERE cardid = ? AND riskcondreasoncodeid IS NULL";
+
+//        BLOQUEO PERMANENTE (estadoDeseado 2)
+        String query2 = "UPDATE SUNNELP3.t_gcard SET riskconditionind = 'T', riskconditiondate = CURRENT_DATE, " +
+                "riskcondreasoncodeid = 2 WHERE cardid = ? ";
+
+//        DESBLOQUEO TEMPORAL (estadoDeseado 0)
+        String query3 = "UPDATE SUNNELP3.t_gcard SET riskconditionind = 'F', riskconditiondate = null, " +
+                "riskcondreasoncodeid = null WHERE cardid = ? AND riskcondreasoncodeid = 1";
+
+        ConnectionHandler connectionHandler = new ConnectionHandler();
+        Connection conexion = connectionHandler.getConnection(remoteJndiSunnel);
+        int result=0;
+
+        switch (estadoDeseado) {
+            case "1":
+                PreparedStatement ps1 = conexion.prepareStatement(query1);
+                ps1.setString(1, numeroTarjeta);
+                result = ps1.executeUpdate();
+                ps1.close();
+                if (result > 0) {
+                    response.setStatusMessage("Proceso exitoso");
+                    response.setStatusCode("00");
+                    response.setStatus("SUCCESS");
+                    log.info(new ObjectMapper().writeValueAsString(response));
+                }else{
+                    response.setStatusMessage("La tarjeta ya se encuentra bloqueada");
+                    response.setStatusCode("400");
+                    response.setStatus("ERROR");
+                    log.info(new ObjectMapper().writeValueAsString(response));
+                }
+                break;
+            case "2":
+                PreparedStatement ps2 = conexion.prepareStatement(query2);
+                ps2.setString(1, numeroTarjeta);
+                result = ps2.executeUpdate();
+                ps2.close();
+                if (result > 0) {
+                    response.setStatusMessage("Proceso exitoso");
+                    response.setStatusCode("00");
+                    response.setStatus("SUCCESS");
+                    log.info(new ObjectMapper().writeValueAsString(response));
+                }else{
+                    response.setStatusMessage("La tarjeta ya se encuentra bloqueada");
+                    response.setStatusCode("400");
+                    response.setStatus("ERROR");
+                    log.info(new ObjectMapper().writeValueAsString(response));
+                }
+                break;
+            case "0":
+                PreparedStatement ps3 = conexion.prepareStatement(query3);
+                ps3.setString(1, numeroTarjeta);
+                result = ps3.executeUpdate();
+                ps3.close();
+                if (result > 0) {
+                    response.setStatusMessage("Proceso exitoso");
+                    response.setStatusCode("00");
+                    response.setStatus("SUCCESS");
+                    log.info(new ObjectMapper().writeValueAsString(response));
+                }else{
+                    response.setStatusMessage("La tarjeta ya se encuentra desbloqueada");
+                    response.setStatusCode("400");
+                    response.setStatus("ERROR");
+                    log.info(new ObjectMapper().writeValueAsString(response));
+                }
+                break;
+            default:
+                response.setStatusMessage("Estado deseado no valido");
+                response.setStatusCode("400");
+                response.setStatus("ERROR");
+                log.info(new ObjectMapper().writeValueAsString(response));
+                break;
+        }
+        conexion.close();
+        return response;
+    }
+
+    public static BloqueoDesbloqueoTarjetaResponse obtenerDatosSiscard(String pais, String numeroTarjeta, String estadoDeseado, String motivo,
+                                                                       String siscardUrl) throws Exception {
+        BloqueoDesbloqueoTarjetaResponse response1 = new BloqueoDesbloqueoTarjetaResponse();
+        JSONObject jsonSend = new JSONObject(); //json a enviar
+        jsonSend.put("country", pais)
+                .put("processIdentifier", "BloqueoDesbloqueoTarjetas")
+                .put("numeroTarjeta", numeroTarjeta)
+                .put("estadodeseado", estadoDeseado)
+                .put("motivoCancelacion", motivo);
+
+        HttpResponse<String> jsonResponse //realizar petición mediante unirest
+                = Unirest.post(siscardUrl.concat("/bloqueoDesbloqueoTarjetas"))
+                .header("Content-Type", "application/json")
+                .body(jsonSend.toString())
+                .asString();
+
+        //capturar respuesta
+        JSONObject response = new JSONObject(jsonResponse
+                .getBody()
+                .replaceAll("\u200B", ""));
+        response1 = new ObjectMapper()
+                .readValue(response.toString(), BloqueoDesbloqueoTarjetaResponse.class);
+
+        log.info(new ObjectMapper().writeValueAsString(response1));
+
+        if(response1!=null){
+            if(response1.getStatusMessage().equals("GRUPO DE USUARIO INHABILITADO P/CAMBIO..")){
+                if(estadoDeseado.equals("00")){
+                    response1.setStatusMessage("La tarjeta ya se encuentra desbloqueada");
+                }
+            }if(response1.getStatusMessage().equals("GRUPO DE USUARIO INHABILITADO P/CAMBIO..")){
+                if(estadoDeseado.equals("28")){
+                    response1.setStatusMessage("La tarjeta ya se encuentra bloqueada");
+                }
+            }
+            if(response1.getStatusMessage().equals("TRANSACCION REALIZADA CON EXITO ........")){
+                    response1.setStatusCode("00");
+                    response1.setStatus("SUCCES");
+                    response1.setStatusMessage("Proceso exitoso");
+            }
+        }
+        return response1;
+    }
+
+    public static XmlObject estructura(BloqueoDesbloqueoTarjetaResponse response1) throws Exception {
         XmlObject result = XmlObject.Factory.newInstance();
         XmlCursor cursor = result.newCursor();
         QName responseQName = new QName(namespace, operationResponse);
+
         cursor.toNextToken();
         cursor.beginElement(responseQName);
-        cursor.insertElementWithText(new QName(namespace, "statusCode"), "00");
-        cursor.insertElementWithText(new QName(namespace, "status"), "SUCCESS");
-        cursor.insertElementWithText(new QName(namespace, "statusMessage"), "Proceso exitoso");
+
+        cursor.insertElementWithText(new QName(namespace, "statusCode"), response1.getStatusCode());
+        cursor.insertElementWithText(new QName(namespace, "status"), response1.getStatus());
+        cursor.insertElementWithText(new QName(namespace, "statusMessage"), response1.getStatusMessage());
         cursor.toParent();
-        log.info("obtenerBloqueoDesbloqueoTarjeta response = [" + result.toString() + "]");
+
+        log.info("obtenerBloqueoDesbloqueoTarjeta response = [" + result + "]");
         return result;
     }
 }
--- src/test/java/com/siman/credisiman/visa/service/BloqueoDesbloqueoTarjetaTest.java
+++ src/test/java/com/siman/credisiman/visa/service/BloqueoDesbloqueoTarjetaTest.java
@@ -7,11 +7,20 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class BloqueoDesbloqueoTarjetaTest {
-    static private String NS = "http://siman.com/BloqueoDesbloqueoTarjeta";
+    static private final String NS = "http://siman.com/BloqueoDesbloqueoTarjeta";
 
     @Test
-    public void obtenerConsultaSubProductosOk() {
-        XmlObject result = BloqueoDesbloqueoTarjeta.obtenerBloqueoDesbloqueoTarjeta("SLV", "4000123456780000","01","Robo", "jndi/SimacSV", "jdbc/ORIONREPOSV", "http://soauat.siman.com:7003/v1/orion", "usuario", "600831, 600831, 600831","P");
+    public void bloquearTarjetaTemporalOk() {
+        XmlObject result = BloqueoDesbloqueoTarjeta.obtenerBloqueoDesbloqueoTarjeta("SV", "6008315500062519","1","M6", "jdbc/SUNTST", "jdbc/ORIONREPOSV", "http://soauat.siman.com:7003/v1/orion", "usuario", "600831, 600831, 600831","P");
+
+        //Status
+        assertEquals("00", ((SimpleValue) result.selectPath( "declare namespace ns='" + NS + "' " + ".//ns:statusCode")[0]).getStringValue());
+        assertEquals("SUCCESS", ((SimpleValue) result.selectPath( "declare namespace ns='" + NS + "' " + ".//ns:status")[0]).getStringValue());
+
+    }
+    @Test
+    public void desbloquearTarjetaTemporalOk() {
+        XmlObject result = BloqueoDesbloqueoTarjeta.obtenerBloqueoDesbloqueoTarjeta("SV", "6008315500062519","0","M6", "jdbc/SUNTST", "jdbc/ORIONREPOSV", "http://soauat.siman.com:7003/v1/orion", "usuario", "600831, 600831, 600831","P");
 
         //Status
         assertEquals("00", ((SimpleValue) result.selectPath( "declare namespace ns='" + NS + "' " + ".//ns:statusCode")[0]).getStringValue());
