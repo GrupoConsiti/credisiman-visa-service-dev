d607e2f9a40b974c14f47dbedceff080811d2bd4
carlossalazar7 <carlossalazar2228@gmail.com>
2023-01-27 21:04:29 -0600

    OSB jar update

--- src/test/java/com/siman/credisiman/visa/service/SolicitudTrasladoCompraEnCuotasTest.java
+++ src/test/java/com/siman/credisiman/visa/service/SolicitudTrasladoCompraEnCuotasTest.java
@@ -7,12 +7,16 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class SolicitudTrasladoCompraEnCuotasTest {
-    private static final String NS = "http://siman.com/ListarPlanesPorProducto";
+    private static final String NS = "http://siman.com/SolicitudTrasladoCompraEnCuotas";
 
     @Test
     public void obtenerConsultaMovimientosPrivadaGTOk() {
-        XmlObject result = ListarPlanesPorProducto.obtenerListarPlanesPorProducto("SV",
-                "01", "jdbc/SUNTSTGT", "jdbc/ORIONREPOSV", "http://soauat.siman.com:7003/v1/orion",
+        XmlObject result = SolicitudTrasladoCompraEnCuotas.obtenerSolicitudTrasladoCompraEnCuotas("SV",
+                "442427223", "6008314502274917","carlos_penate@siman.com",
+                "22224444","57","63","0.10","20230126",
+                "02","840","DUI","Carlos Salazar",
+                "jdbc/SUNTSTGT", "jdbc/ORIONREPOSV",
+                "http://soauat.siman.com:7003/v1/orion",
                 "usuario", "600831, 600831, 600831");
 
         //Status
